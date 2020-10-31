0a2f8f1dc9173a6d49b1e96c2847a634695e7fc1
okamoto8907 <43702077+okamoto8907@users.noreply.github.com>
2020-10-31 22:54:15 +0900

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -9,3 +9,16 @@ function mouseOff(){
   var obj = document.getElementById("map001");
   obj.src = "map001.jpeg";
 }
+
+//プラスボタン
+var num = 0;
+function counter(){
+num++;
+document.getElementById("plus-button").innerHTML = num;
+}
+
+//画像配列
+img = new Array();
+img[0] = "map001.jpg";
+img[1] = "map002.jpg";
+img[2] = "map003.jpg";
