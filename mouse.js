b15a890952869fc061bd5078b050f5e5ca5e9764
okamoto8907 <43702077+okamoto8907@users.noreply.github.com>
2020-10-31 20:52:19 +0900

    Add files via upload

---
+++ mouse.js
@@ -0,0 +1,11 @@
+//マウスオーバー時の処理
+function mouseOn(){
+  var obj = document.getElementById("map001");
+  obj.src = “map002.jpeg”;
+}    
+
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("map001");
+  obj.src = "map001.jpeg";
+}
