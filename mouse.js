c3f6c1c55f90a816658248926a3b5cea2a96f1a8
okamoto8907 <43702077+okamoto8907@users.noreply.github.com>
2020-11-09 20:03:22 +0900

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -22,3 +22,26 @@ img = new Array();
 img[0] = "map001.jpeg";
 img[1] = "map002.jpeg";
 img[2] = "map003.jpeg";
+
+
+
+// ハンバーガメニュー
+function hamburger() {
+　　document.getElementById('line1').classList.toggle('linea');
+　　document.getElementById('line2').classList.toggle('lineb');
+　　document.getElementById('line3').classList.toggle('linec');
+　　document.getElementById('target').classList.toggle('slidex');
+　　document.getElementById('nav_f').classList.toggle('fadein');
+}
+
+document.getElementById('target').addEventListener('click', function () {
+　　hamburger();
+});
+
+let list = document.getElementsByClassName('list');
+
+for (let i = 0; i < list.length; i++) {
+　　list[i].addEventListener('click', function () {
+　　　　hamburger();
+    });
+}
