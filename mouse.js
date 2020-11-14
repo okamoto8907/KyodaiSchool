c449bc0a4d5c91dc306da8a3a01dbb72b72e14dc
okamoto8907 <43702077+okamoto8907@users.noreply.github.com>
2020-11-14 20:40:39 +0900

    Update mouse.js

--- mouse.js
+++ mouse.js
@@ -9,14 +9,3 @@ function mouseOff(){
   var obj = document.getElementById("map001");
   obj.src = "map001.jpeg";
 }
-
-// ハンバーガメニュー
-function hamburger() {
-  document.getElementById('line1').classList.toggle('line_1');
-  document.getElementById('line2').classList.toggle('line_2');
-  document.getElementById('line3').classList.toggle('line_3');
-  document.getElementById('nav').classList.toggle('in');
-}
-document.getElementById('hamburger').addEventListener('click' , function () {
-  hamburger();
-} );
