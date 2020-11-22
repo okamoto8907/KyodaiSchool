1e565e69c2b4e99a4547bc615c2c262c5a601d90
okamoto8907 <43702077+okamoto8907@users.noreply.github.com>
2020-11-22 22:24:57 +0900

    Update textchange.js

--- textchange.js
+++ textchange.js
@@ -1,12 +1,13 @@
 var today = new Date();
 var day = today.getDate();
 var hour = today.getHour();
-var flag =day*hour
+var minutes = today.getMinutes();
+var flag =day+hour+minutes
 
-if(360>=flag>=0){
+if(40>=flag>=0){
 document.write("<h2>★今日のフレーズ★〜謙虚な心を持つ〜人からの言葉や物事をあるがままに素直に受け入れる事は、自身の成長につながる</h2>");
 }
-else if(5400>=flag>360){
+else if(80>=flag>40){
 document.write("<h2>★今日のフレーズ★〜心の痛みがわかる人〜経験もなく理解するこということは難しく、限られた時間であらゆることを経験することは不可能である。経験により培われた想像力により、自分の懐が深くなっていく</h2>");
         
 }else{
